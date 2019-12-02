61166be32ac0c6b5841803ad456ccbc404fdc7d1
gdm-1718-indymeer <indymeer@student.arteveldehs.be>
2019-12-02 15:20:20 +0100

    beta

---
+++ assets/js/firebase-details.js
@@ -0,0 +1,16 @@
+// Initialize firebase here
+//! These details are only used for the demo web app
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBH1e1oVzUSAx89LjfbXAnrTAb-MzJoEJ8",
+    authDomain: "slaapwandelproject.firebaseapp.com",
+    databaseURL: "https://slaapwandelproject.firebaseio.com",
+    projectId: "slaapwandelproject",
+    storageBucket: "slaapwandelproject.appspot.com",
+    messagingSenderId: "1044491911094",
+    appId: "1:1044491911094:web:61774d17d9f034a3fa4b17"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
