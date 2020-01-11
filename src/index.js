7d86f4885e8446e4cf1d6f8f30c7c3355fb32a6d
Imran <imramuha@student.arteveldehs.be>
2020-01-11 18:30:56 +0100

    https to http

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import app from './app';
 
 const { PORT = 8080 } = process.env;
-app.listen(PORT, () => console.log(`Listening on port https://127.0.0.1:${PORT}`)); // eslint-disable-line no-console
+app.listen(PORT, () => console.log(`Listening on port http://127.0.0.1:${PORT}`)); // eslint-disable-line no-console
